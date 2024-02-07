b9f1d1130c2aaa165e3d202caea7c13bc47c09ac
¨hamzatahirghauri¨ <hamzatahirghauri@gmail.com>
2024-02-07 21:19:23 +0200

    Built a responsive layout

--- Game-Discovery-App/src/App.tsx
+++ Game-Discovery-App/src/App.tsx
@@ -1,12 +1,28 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <div>
-      <Button colorScheme='blue'>Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="green">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="violet">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="purple">
+          Main
+        </GridItem>
+      </Grid>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
