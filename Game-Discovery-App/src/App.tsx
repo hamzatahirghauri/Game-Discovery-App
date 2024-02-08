7777e73ae4a70ce916774919dbd9219150776466
¨hamzatahirghauri¨ <hamzatahirghauri@gmail.com>
2024-02-08 18:41:45 +0200

    Changed color of Navbar

--- Game-Discovery-App/src/App.tsx
+++ Game-Discovery-App/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem area="nav">
+        <GridItem area="nav" bg={"pink"}>
           <NavBar />
         </GridItem>
         <Show above="lg">
